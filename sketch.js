c530523bf7976605c1b6dce8821e2d62fdce8e35
Yannic Fréson <yannic.freson1@gmail.com>
2020-04-10 17:14:11 +0200

    added text animation

--- sketch.js
+++ sketch.js
@@ -1,10 +1,19 @@
 var streams = [];
 var fadeInterval = 2;
-var symbolSize = 25;
+var symbolSize = 20;
 var opacity = 0;
-var message = "Lorem ipsum dolor sit amet";
+var message = "Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+var messageAsArray = message.split('');
+var displayedMessage = "";
+var currentCharToType = 0;
+var letterSize = 14;
+var hasStarted = false;
+var hasDrawnMessageBox = false;
+var finished = false;
 
 function setup() {
+  console.log(message.length);
+  frameRate(60);
   createCanvas(
     window.innerWidth,
     window.innerHeight
@@ -20,17 +29,21 @@ function setup() {
   }
 
   textFont('Consolas');
-  textSize(symbolSize);
 }
 
 function draw() {
+  textSize(symbolSize);
   background(0, 150);
   streams.forEach(function (stream) {
     stream.render();
   });
-  /* MessageBox();
-  TypeWriter();
-  DisplayMessage(); */
+  messageBox();
+  displayMessage();
+  if (hasStarted == false) {
+    console.log('start message');
+    constructMessage();
+  }
+  hasStarted = true;
 }
 
 function Symbol(x, y, speed, first, opacity) {
@@ -65,8 +78,8 @@ function Symbol(x, y, speed, first, opacity) {
 
 function Stream() {
   this.symbols = [];
-  this.totalSymbols = round(random(5, 35));
-  this.speed = random(2, 10);
+  this.totalSymbols = round(random(5, 18));
+  this.speed = random(1, 5);
 
   this.generateSymbols = function (x, y) {
     var opacity = 255;
@@ -101,7 +114,8 @@ function Stream() {
   }
 }
 
-function MessageBox() {
+function messageBox() {
+  console.log('drawing message box');
   this.opacity = opacity;
   var boxX1 = (width / 8);
   var boxY1 = (height / 5);
@@ -115,27 +129,50 @@ function MessageBox() {
   rect(boxX1, boxY1, boxX2, boxY2);
 }
 
-function DisplayMessage() {
-  var boxX1 = (width / 8);
-  var boxY1 = (height / 5);
-  var boxX2 = 6 * (width / 8);
-  var boxY2 = 3 * (height / 5);
-  var textX1 = boxX1 + symbolSize * 2;
-  var textY1 = boxY1 + symbolSize * 2;
-  var textX2 = boxX2 - symbolSize * 2;
-  var textY2 = boxY2 - symbolSize * 2;
-  this.message = message;
+function constructMessage() {
+  console.log('displaying message')
+  this.displayedMessage = displayedMessage;
 
-  /* var captionLength = 0;
-  var caption = 'Lorem ipsum dolor';
+  setTimeout(type, 7000);
 
-  if (captionLength < caption.length + 1) {
-    setTimeout('type()', 50);
-  } else {
-    captionLength = 0;
-    caption = '';
-  } */
+  function type() {
+    console.log('type')
+    nextChar();
+    function nextChar() {
+      this.currentCharToType = currentCharToType;
+      if (currentCharToType < messageAsArray.length) {
+        displayedMessage += messageAsArray[currentCharToType];
+        console.log(displayedMessage);
+        currentCharToType++;
+      } else if (currentCharToType >= messageAsArray.length) {
+        finished = true
+      }
+    }
+    if (finished == false) {
+      setTimeout(type, round(random(0, 10)));
+    }
+  }
+}
 
-  fill(255);
-  text(message, textX1 + (5 * symbolSize), textY1, textX2 - (6 * symbolSize), textY2);
+function displayMessage() {
+  textSize(letterSize);
+  this.letterSize = letterSize;
+  this.displayedMessage = displayedMessage;
+  var header = "Neo_K >"
+
+  var headerX1 = width / 7;
+  var headerY1 = height / 4;
+  var headerX2 = headerX1 + (letterSize * 5);
+  var headerY2 = headerY1 + letterSize;
+
+  var textX1 = headerX1 + ((header.length - 2) * letterSize);
+  var textY1 = height / 4;
+  var textX2 = width - (2 * (width / 7)) - (letterSize * 4);
+  var textY2 = height - (2 * (height / 4));
+
+  fill(0, 255, 80);
+  if (frameCount > 300) {
+    text(header, headerX1, headerY1, headerX2, headerY2);
+  }
+  text(displayedMessage, textX1, textY1, textX2, textY2);
 }
